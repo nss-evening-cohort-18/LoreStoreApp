c36cfb0ae89bdc1c377de3e03d60a104b74a2f6c
whollahan <bakuhf@gmail.com>
2022-10-22 11:41:47 -0500

    added paymentmethod model

---
+++ LoreStoreAPI/Models/PaymentMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace LoreStoreAPI.Models
+{
+    public class PaymentMethod
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationMonth { get; set; }
+        public string Expirationyear { get; set; }
+        public string Cvv { get; set; }
+    }
+}
