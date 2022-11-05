50c993fd8de13e2dd77d5df0e6f8a35a4d4f57ea
Nick Stewart <nick.stewart@array.com>
2022-11-05 12:15:50 -0500

    backend fb user register

--- front-end/src/utils/auth.js
+++ front-end/src/utils/auth.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const signIn = () => {
+const signIn = async () => {
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider);
+  const userLogin = await firebase.auth().signInWithPopup(provider);
+  console.log(userLogin);
+  await fetch(`https://localhost:7294/api/UserAuth/${userLogin.user.uid}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${userLogin.user.Aa}`,
+    },
+  });
 };
 
 const signOut = () => {
