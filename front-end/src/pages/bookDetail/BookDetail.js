496655d2cc28fd252cedc2b02df712e4cba301d4
Sarah Maginnis <sarahemaginnis@gmail.com>
2022-11-14 18:10:10 -0600

    book detail edits; more in-line comments

--- front-end/src/pages/bookDetail/BookDetail.js
+++ front-end/src/pages/bookDetail/BookDetail.js
@@ -54,14 +54,14 @@ export const BookDetail = () => {
             <h3 className="book_description_header">Description</h3>
             <p className="book_description">{book.description}</p>
             <h3 className="book_details">Product Details</h3>
-            <p className="book_price">Price ${book.price}</p>
+            <p className="book_price">Price ${book.price}</p> {/* Need to modify price so that it displays decimals, even if it's an even number, e.g. $81.00 instead of $81 */}
             <p className="book_quantity">
               Quantity Available: {book.inventoryQuantity}
             </p>
             <p className="book_publication_date">
-              Publication Date: {book.datePublished}
+              Publication Date: {book.datePublished} {/* Need to modify the date so that it's presented mm/dd/yyyy */}
             </p>
-            <p className="book_genres">Genres: {book.subGenre}</p>
+            <p className="book_genres">Genres: {book.subGenre}</p> {/* Link out genres so that when they're clicked they take you to a filtered view of books that matches that genre */}
           </Col>
         </Row>
       </Container>
